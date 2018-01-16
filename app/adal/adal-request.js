0c05ecbc3ce6a634c72c381ef4084b1a20b08e12
Rohit Narula <ronaru@microsoft.com>
2018-01-16 12:49:57 -0800

    fixed processAdalCallback to work with Adaljs 1.0.16

--- app/adal/adal-request.js
+++ app/adal/adal-request.js
@@ -5,43 +5,53 @@ var adalConfig = require('./adal-config');
 var _adal = new AuthenticationContext(adalConfig);
 var _oauthData = { isAuthenticated: false, userName: '', loginError: '', profile: '' };
 
-var processAdalCallback = function() {
+var processAdalCallback = function () {
   var hash = window.location.hash;
 
   if (_adal.isCallback(hash)) {
-    // callback can come from login or iframe request
-    var requestInfo = _adal.getRequestInfo(hash);
-    _adal.saveTokenFromHash(requestInfo);
-    window.location.hash = '';
+    if (window.parent && window.parent._adalInstance) {
+      _adal = window.parent._adalInstance;
+    }
 
-    if (requestInfo.requestType !== _adal.REQUEST_TYPE.LOGIN) {
-      if (window.parent.AuthenticationContext === 'function' && window.parent.AuthenticationContext()) {
-        _adal.callback = window.parent.AuthenticationContext().callback;
-      }
-      if (requestInfo.requestType === _adal.REQUEST_TYPE.RENEW_TOKEN) {
-        _adal.callback = window.parent.callBackMappedToRenewStates[requestInfo.stateResponse];
+    if (_adal._openedWindows.length > 0) {
+      var lastWindow = _adal._openedWindows[_adal._openedWindows.length - 1];
+      if (lastWindow.opener &&
+        lastWindow.opener._adalInstance) {
+        _adal = lastWindow.opener._adalInstance;
       }
     }
 
+    // callback can come from login or iframe request
+    _adal.verbose('Processing the hash: ' + hash);
+    var requestInfo = _adal.getRequestInfo(hash);
+    _adal.saveTokenFromHash(requestInfo);
+    window.location.hash = '';
+    // Return to callback if it is sent from iframe
+    var callback = _adal._callBackMappedToRenewStates[requestInfo.stateResponse] || _adal.callback;
+
     if (requestInfo.stateMatch) {
-      if (typeof _adal.callback === 'function') {
+      if (callback && typeof callback === 'function') {
         // Call within the same context without full page redirect keeps the callback
         if (requestInfo.requestType === _adal.REQUEST_TYPE.RENEW_TOKEN) {
           // Idtoken or Accestoken can be renewed
           if (requestInfo.parameters['access_token']) {
-            _adal.callback(_adal._getItem(_adal.CONSTANTS.STORAGE.ERROR_DESCRIPTION), requestInfo.parameters['access_token']);
-            return;
-          } else if (requestInfo.parameters['id_token']) {
-            _adal.callback(_adal._getItem(_adal.CONSTANTS.STORAGE.ERROR_DESCRIPTION), requestInfo.parameters['id_token']);
+            callback(_adal._getItem(_adal.CONSTANTS.STORAGE.ERROR_DESCRIPTION), requestInfo.parameters['access_token']);
             return;
           }
+          else {
+            if (requestInfo.parameters['id_token']) {
+              callback(_adal._getItem(_adal.CONSTANTS.STORAGE.ERROR_DESCRIPTION), requestInfo.parameters['id_token']);
+              return;
+            }
+          }
         }
-      } else {
+      }
+      else {
         // normal full login redirect happened on the page
         updateDataFromCache(_adal.config.loginResource);
         if (_oauthData.userName) {
           //IDtoken is added as token for the app
-          window.setTimeout(function() {
+          window.setTimeout(function () {
             updateDataFromCache(_adal.config.loginResource);
             // redirect to login requested page
             var loginStartPage = _adal._getItem(_adal.CONSTANTS.STORAGE.START_PAGE);
@@ -55,7 +65,7 @@ var processAdalCallback = function() {
   }
 }
 
-var updateDataFromCache = function(resource) {
+var updateDataFromCache = function (resource) {
   // only cache lookup here to not interrupt with events
   var token = _adal.getCachedToken(resource);
   _oauthData.isAuthenticated = token !== null && token.length > 0;
@@ -65,14 +75,14 @@ var updateDataFromCache = function(resource) {
   _oauthData.loginError = _adal.getLoginError();
 };
 
-var isAuthenticated = function() {
+var isAuthenticated = function () {
   var deferred = q.defer();
 
   updateDataFromCache(_adal.config.loginResource);
   if (!_adal._renewActive && !_oauthData.isAuthenticated && !_oauthData.userName) {
     if (!_adal._getItem(_adal.CONSTANTS.STORAGE.FAILED_RENEW)) {
       // Idtoken is expired or not present
-      _adal.acquireToken(_adal.config.loginResource, function(error, tokenOut) {
+      _adal.acquireToken(_adal.config.loginResource, function (error, tokenOut) {
         if (error) {
           _adal.error('adal:loginFailure', 'auto renew failure');
           deferred.reject();
@@ -99,11 +109,11 @@ var isAuthenticated = function() {
   return deferred.promise;
 }
 
-var makeRequest = function(settings) {
+var makeRequest = function (settings) {
   var deferred = q.defer();
 
   var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = function () {
     if (this.readyState === 4) {
       if (this.status === 200) {
         deferred.resolve(this.response);
@@ -125,10 +135,10 @@ var makeRequest = function(settings) {
   return deferred.promise;
 }
 
-var adalRequest = function(settings) {
+var adalRequest = function (settings) {
   var deferred = q.defer();
 
-  isAuthenticated().then(function() {
+  isAuthenticated().then(function () {
     var resource = _adal.getResourceForEndpoint(settings.url);
 
     if (!resource) {
@@ -161,7 +171,7 @@ var adalRequest = function(settings) {
         deferred.reject();
       }
       else if (isEndpoint) {
-        _adal.acquireToken(resource, function(error, tokenOut) {
+        _adal.acquireToken(resource, function (error, tokenOut) {
           if (error) {
             deferred.reject();
             _adal.error(error);
@@ -179,7 +189,7 @@ var adalRequest = function(settings) {
         });
       }
     }
-  }, function() {
+  }, function () {
     _adal.login();
   })
 
